f7a8abfbb6b3d212e3bbd29abc9b8cba41582b59
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-01-03 12:42:17 +0800

    Configure DayNight modes

--- app/src/main/java/io/github/zwliew/zwliew/MainApplication.kt
+++ app/src/main/java/io/github/zwliew/zwliew/MainApplication.kt
@@ -1,12 +1,15 @@
 package io.github.zwliew.zwliew
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
 
         if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
