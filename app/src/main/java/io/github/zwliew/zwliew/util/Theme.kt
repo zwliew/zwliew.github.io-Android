0971d8040a83e296d82eee5a69c89ac19d334ef6
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-01-04 15:49:35 +0800

    Theme: Fix theme update method
    We weren't actually changing the night mode setting for the app.

--- app/src/main/java/io/github/zwliew/zwliew/util/Theme.kt
+++ app/src/main/java/io/github/zwliew/zwliew/util/Theme.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatDelegate
 import io.github.zwliew.zwliew.destinations.settings.ThemeValue
 
 fun updateTheme(themeValue: String) {
-    when (themeValue) {
-        ThemeValue.LIGHT.value -> AppCompatDelegate.MODE_NIGHT_NO
-        ThemeValue.DARK.value -> AppCompatDelegate.MODE_NIGHT_YES
-        ThemeValue.AUTO.value -> AppCompatDelegate.MODE_NIGHT_AUTO
-        ThemeValue.SYSTEM.value -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-        else -> throw IllegalArgumentException()
-    }
+    AppCompatDelegate.setDefaultNightMode(
+        when (themeValue) {
+            ThemeValue.LIGHT.value -> AppCompatDelegate.MODE_NIGHT_NO
+            ThemeValue.DARK.value -> AppCompatDelegate.MODE_NIGHT_YES
+            ThemeValue.AUTO.value -> AppCompatDelegate.MODE_NIGHT_AUTO
+            ThemeValue.SYSTEM.value -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            else -> throw IllegalArgumentException()
+        }
+    )
 }
